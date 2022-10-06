9956496b0af732ea85ddd5fff10100b6c87fc107
muqsitadam <muqsitadam@gmail.com>
2022-10-06 13:34:53 +0100

    Box-shadow fix invest page

--- app.js
+++
@@ -1,6 +0,0 @@
-// const cards = document.querySelectorAll('.card')
-// cards.forEach((card) => {
-//     card.addEventListener('mouseover', () => {
-//         document.querySelector('.last-item').classList.add('move-right')
-//     })
-// })
